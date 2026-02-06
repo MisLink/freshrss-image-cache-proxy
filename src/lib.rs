f667457f7d824d90d38f6dd1c7b4f43220bea8cf
MisLink <gjq.uoiai@outlook.com>
2026-02-07 01:17:17 +0800

    Update
    Signed-off-by: MisLink <gjq.uoiai@outlook.com>

---
+++ src/lib.rs
@@ -0,0 +1,100 @@
+use std::collections::HashMap;
+
+use sha2::{Digest, Sha256};
+use worker::{
+    console_debug, event, Context, Data, Env, Error, Fetch, Object, Request, Response,
+    ResponseBody, Result, RouteContext, Router, Url,
+};
+
+fn get_r2_key(url: &str) -> String {
+    let hash = Sha256::digest(url.as_bytes());
+    let elen = base16ct::encoded_len(&hash);
+    let mut dst = vec![0u8; elen];
+    let hex = base16ct::lower::encode_str(&hash, &mut dst).expect("dst length is correct");
+    format!("{}/{}/{}", &hex[0..2], &hex[2..4], &hex[4..])
+}
+
+async fn put_in_r2(ctx: &RouteContext<()>, url: &str, res: Response) -> Result<()> {
+    let key = get_r2_key(url);
+    let bucket = ctx.bucket("R2_BINDING")?;
+    let r = bucket.head(&key).await?;
+    if r.is_some() {
+        console_debug!("object {} already exists in R2", &key);
+        return Ok(());
+    }
+    let value = match res.body().clone() {
+        ResponseBody::Empty => Data::Empty,
+        ResponseBody::Body(items) => Data::Bytes(items),
+        ResponseBody::Stream(readable_stream) => Data::ReadableStream(readable_stream),
+    };
+    let _ = bucket
+        .put(&key, value)
+        .custom_metadata(HashMap::from([("url".to_string(), url.to_string())]))
+        .execute()
+        .await?;
+    Ok(())
+}
+
+async fn get_from_r2(ctx: &RouteContext<()>, url: &str) -> Result<Option<Object>> {
+    let key = get_r2_key(url);
+    let bucket = ctx.bucket("R2_BINDING")?;
+    bucket.get(&key).execute().await
+}
+
+async fn cache_url(ctx: &RouteContext<()>, url_str: &str) -> Result<Response> {
+    let url = Url::parse(url_str)?;
+    let mut res = Fetch::Url(url).send().await?;
+    match res.status_code() {
+        200..300 => {
+            put_in_r2(ctx, url_str, res.cloned()?).await?;
+            Ok(res)
+        }
+        400.. => {
+            if let Some(obj) = get_from_r2(ctx, url_str).await? {
+                if let Some(body) = obj.body() {
+                    console_debug!("object found in R2, returning cached response");
+                    return Response::from_body(body.response_body()?);
+                }
+            }
+            let fallback_url = ctx.env.var("FALLBACK_URL")?.to_string();
+            let url = Url::parse(&fallback_url)?;
+            Fetch::Url(url).send().await
+        }
+        _ => Err(Error::from("unexpected status code from origin")),
+    }
+}
+
+async fn get(req: Request, ctx: RouteContext<()>) -> Result<Response> {
+    let q = req
+        .url()?
+        .query_pairs()
+        .find(|(k, _)| k == "url")
+        .map(|(_, v)| v.into_owned());
+    let url = q.ok_or_else(|| Error::from("missing url parameter"))?;
+    cache_url(&ctx, &url).await
+}
+
+#[derive(serde::Deserialize)]
+struct PostRequest {
+    url: String,
+    access_token: String,
+}
+
+async fn post(mut req: Request, ctx: RouteContext<()>) -> Result<Response> {
+    let req: PostRequest = req.json().await?;
+    let api_token = ctx.env.var("API_TOKEN")?.to_string();
+    if req.access_token != api_token {
+        return Response::error("invalid access token", 403);
+    }
+    cache_url(&ctx, &req.url).await?;
+    Response::empty()
+}
+
+#[event(fetch)]
+async fn fetch(req: Request, env: Env, _ctx: Context) -> Result<Response> {
+    Router::new()
+        .get_async("/", get)
+        .post_async("/", post)
+        .run(req, env)
+        .await
+}
